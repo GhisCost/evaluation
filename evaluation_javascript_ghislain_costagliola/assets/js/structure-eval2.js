ee172dccad9c5aa0a5203ef6314f9f6d9149eaf8
unknown <ghis.cost@gmail.com>
2025-07-25 16:26:02 +0200

    fignolage de l'eval JS

--- evaluation_javascript_ghislain_costagliola/assets/js/structure-eval2.js
+++ evaluation_javascript_ghislain_costagliola/assets/js/structure-eval2.js
@@ -8,16 +8,16 @@ let liste = document.querySelector("#tasklist");
 let nouvelleTache = document.querySelector("#taskinput");
 let btnAjout = document.querySelector("#addtaskbutton");
 
-// fonction qui ajoute une chaine de caractère à la fin du tableau
+// Fonction qui ajoute une chaine de caractère à la fin du tableau.
 
 function ajouterTache(str) {
   if (nouvelleTache.value == "") {
-    return; // Empeche d'ajouter une tache vide si rien n'est écrit
+    return; // Empeche d'ajouter une tache vide si rien n'est écrit.
   }
   listDeTaches.push(str);
 }
 
-// fonction qui affiche la liste de tache dans le ul
+// Fonction qui affiche la liste de tache dans le ul.
 
 function afficherTaches() {
   listDeTaches.forEach((taches) => {
@@ -25,17 +25,17 @@ function afficherTaches() {
   });
 }
 
-// Fonction qui supprime une tache du tableau
+// Fonction qui supprime une tache du tableau.
 
 function supprimerTache(index) {
   listDeTaches.splice(index, 1);
-};
+}
 
 // Ecouteurs d'événements
 
 btnAjout.addEventListener("click", function () {
   if (nouvelleTache.value == "") {
-    return; // Empeche d'ajouter une tache vide si rien n'est écrit
+    return; // Empeche d'ajouter une tache vide si rien n'est écrit.
   }
   ajouterTache(nouvelleTache.value);
 
@@ -55,39 +55,38 @@ btnAjout.addEventListener("click", function () {
   btnSupp.addEventListener("click", function () {
     let i = listDeTaches.indexOf(this.parentElement);
     supprimerTache(i);
-    this.parentElement.remove(); 
+    this.parentElement.remove();
   });
-  nouvelleTache.value = ""; // Vide l'input une fois l'ajout effectuer
-
+  nouvelleTache.value = ""; // Vide l'input une fois l'ajout effectuer.
 });
 
-// Faire la même chose mais avec la touche "Entrée" au lieu du bouton ajouter
+// Faire la même chose mais avec la touche "Entrée" au lieu du bouton "ajouter".
 
-nouvelleTache.addEventListener('keydown',function(event){
-    if(event.key == "Enter"){
-        if (nouvelleTache.value == "") {
-    return; 
-  }
-  ajouterTache(nouvelleTache.value);
-
-  let li = document.createElement("li");
-  li.textContent = nouvelleTache.value;
-  li.classList.add("tache");
-
-  let btnSupp = document.createElement("button");
-  btnSupp.classList.add("btnsupp");
-  btnSupp.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-
-  afficherTaches();
-
-  li.appendChild(btnSupp);
-  liste.appendChild(li);
-
-  btnSupp.addEventListener("click", function () {
-    let i = listDeTaches.indexOf(this.parentElement);
-    supprimerTache(i);
-    this.parentElement.remove(); 
-  });
-  nouvelleTache.value = ""; 
+nouvelleTache.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    if (nouvelleTache.value == "") {
+      return;
     }
-})
+    ajouterTache(nouvelleTache.value);
+
+    let li = document.createElement("li");
+    li.textContent = nouvelleTache.value;
+    li.classList.add("tache");
+
+    let btnSupp = document.createElement("button");
+    btnSupp.classList.add("btnsupp");
+    btnSupp.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+    afficherTaches();
+
+    li.appendChild(btnSupp);
+    liste.appendChild(li);
+
+    btnSupp.addEventListener("click", function () {
+      let i = listDeTaches.indexOf(this.parentElement);
+      supprimerTache(i);
+      this.parentElement.remove();
+    });
+    nouvelleTache.value = "";
+  }
+});
